792c02f7cf53526eb7cca78ae774a48695f57aeb
Nick DeGroot <nick.degroot@streem.pro>
2020-03-08 01:53:30 -0800

    Initial AR bootstrap

---
+++ app/src/main/java/com/nickthegroot/wcwp10a/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nickthegroot.wcwp10a
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

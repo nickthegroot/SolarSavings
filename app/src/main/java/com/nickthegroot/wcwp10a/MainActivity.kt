de97c939ff418da46fe3e31c83ebba43aca09e80
Nick DeGroot <nick.degroot@streem.pro>
2020-03-08 17:43:45 -0700

    Place solar on tap

--- app/src/main/java/com/nickthegroot/wcwp10a/MainActivity.kt
+++ app/src/main/java/com/nickthegroot/wcwp10a/MainActivity.kt
@@ -1,12 +1,53 @@
 package com.nickthegroot.wcwp10a
 
+import android.content.ContentResolver
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.google.ar.core.Anchor
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fragment: ArFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fragment = supportFragmentManager.findFragmentById(R.id.sceneform_fragment) as ArFragment
+        fragment.setOnTapArPlaneListener { hitResult, plane, motionEvent ->
+            val anchor = hitResult.createAnchor()
+
+            ModelRenderable
+                .builder()
+                .setSource(this, R.raw.solar_battery)
+                .build()
+                .handle { renderable, throwable ->
+                    if (throwable != null) {
+                        AlertDialog.Builder(this)
+                            .setTitle("Model Render Error")
+                            .setMessage(throwable.message)
+                            .create()
+                            .show()
+
+                        return@handle
+                    }
+
+                    addNodeToScene(anchor, renderable)
+                }
+        }
+    }
+
+    fun addNodeToScene(anchor: Anchor, renderable: ModelRenderable) {
+        val anchorNode = AnchorNode(anchor)
+        val node = TransformableNode(fragment.transformationSystem)
+        node.renderable = renderable
+        node.setParent(anchorNode)
+        fragment.arSceneView.scene.addChild(anchorNode)
+        node.select()
     }
 }
